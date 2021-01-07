cea99f9e2153fabe897053b01fb598be3765b003
user <user@MacBook-Pro-de-user-2.local>
2021-01-07 13:51:46 -0500

    usestyles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import Details from './components/Details/Details'
+import {Grid} from '@material-ui/core'
+import  useStyles from './styles'
 
 function app() {
+    const classes = useStyles();
     return (
         <div>
-            hello world
+            <Grid className= {classes.Grid} container spacing={0} alignItems="center" justify="center" style ={{ height: '100vh'}}>
+            <Grid item xs={12} sm={4}>
+                <Details />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+                Main
+            </Grid>
+            <Grid item xs={12} sm={4}>
+                <Details />
+            </Grid>
+            </Grid>
+
         </div>
     );
 }
